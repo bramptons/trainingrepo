2aa992261fb4bc4f5de466bf32791ed2ee9e24c0
Abraham Glover <abesphone@live.co.uk>
2017-06-01 14:36:53 +0100

    updated runbook

--- runbook/workflow.ps1
+++ runbook/workflow.ps1
@@ -1,1 +1,18 @@
-﻿
+﻿workflow Test-Workflow
+{
+    Param(
+    [string]$VMName,
+    [string]$ResourceGroupName
+    )
+
+    Start-AzureRmVM -Name $VMName -ResourceGroupName $ResourceGroupName
+    #workflow checkpoint, in the event of an exception being thrown continue from here - a break point
+    Checkpoint-Workflow
+    #create the following vms in parallel
+    Parallel
+    {
+        Start-AzureRmVM -Name $vm0 -ResourceGroupName $rg1
+        Start-AzureRmVM -Name $vm1 -ResourceGroupName $rg1
+    }
+
+}
